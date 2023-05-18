efb57cfab8649427e49c98ca0221a5f34c59f378
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-05-18 19:03:33 +0400

    Added ZIP class and ZipFileTest

--- src/test/java/com/zaychikov/tests/files_tests/ZipFileTest.java
+++ src/test/java/com/zaychikov/tests/files_tests/ZipFileTest.java
@@ -1,6 +1,26 @@
 package com.zaychikov.tests.files_tests;
 
+import net.lingala.zip4j.exception.ZipException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static com.zaychikov.tests.utils.Files.readTextFromPath;
+import static com.zaychikov.tests.utils.Zip.unzip;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ZipFileTest {
+    @Test
+    void zipWithPasswordTest() throws IOException, ZipException {
+        String zipFilePath = "./src/test/resources/zip_doc_with_password.zip";
+        String unzipFolderPath = "./src/test/resources/unzip";
+        String zipPassword = "123";
+        String unzipTxtFilePath = "./src/test/resources/unzip/readme.txt";
+        String expectedData = "this is a readme.txt file!";
 
-
+        unzip(zipFilePath, unzipFolderPath, zipPassword);
+        String actualData = readTextFromPath(unzipTxtFilePath);
+        assertThat(actualData, containsString(expectedData));
+    }
 }
