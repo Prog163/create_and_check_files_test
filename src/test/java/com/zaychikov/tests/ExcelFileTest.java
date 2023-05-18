67dffdd4daa68b6320b656ec6819e7fe9535fa9e
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-05-18 11:25:30 +0400

    Added changes

--- src/test/java/com/zaychikov/tests/ExcelFileTest.java
+++ src/test/java/com/zaychikov/tests/ExcelFileTest.java
@@ -7,18 +7,18 @@ import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 
 public class ExcelFileTest {
 
-    String fileName = "excel_doc.xls";
-    String cellNameValue = "Bob";
-    String cellAgeValue = "Age";
+    private String fileName = "excel_doc.xls";
+    private String NameValue = "Bob";
+    private String AgeValue = "Age";
 
     @Test
     void xlsFileTest() throws Exception {
         try (InputStream stream = getClass().getClassLoader().getResourceAsStream(fileName)) {
             XLS xlsFile = new XLS(stream);
             assertThat(xlsFile.excel.getSheetAt(0).getRow(1).getCell(0).getStringCellValue())
-                    .isEqualTo(cellNameValue);
+                    .isEqualTo(NameValue);
             assertThat(xlsFile.excel.getSheetAt(0).getRow(0).getCell(1).getStringCellValue())
-                    .isEqualTo(cellAgeValue);
+                    .isEqualTo(AgeValue);
         }
     }
 }
